d5067ec749ce20aac5b66cd01bcdb293167bfd1e
Cris Jr <cristianoandre96@gmail.com>
2022-05-14 17:17:39 -0300

    Inserindo cursos com POST

--- src/main/java/com/andcris/crudspring/controller/CourseController.java
+++ src/main/java/com/andcris/crudspring/controller/CourseController.java
@@ -2,9 +2,14 @@ package com.andcris.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andcris.crudspring.model.Course;
@@ -22,6 +27,16 @@ public class CourseController {
 	@GetMapping
 	public @ResponseBody List<Course> list() {
 		return courseRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public /*ResponseEntity<Course>*/ Course create(@RequestBody Course course) {
+		/*
+		 * return ResponseEntity.status(HttpStatus.CREATED)
+		 * .body(courseRepository.save(course));
+		 */
+		return courseRepository.save(course);
 	}
 
 }
